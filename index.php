2b7badf960dddb4901676756327febcbbc5b0a21
dvanruyven <dvruyven@gmail.com>
2016-09-15 12:15:28 +0200

    Added repository
    -

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+  <meta name="description" content=""/>
+  <meta charset="UTF-8"/>
+  <link rel="stylesheet" href="style.css"/>
+  <title>MVC</title>
+</head>
+<body>
+<h1>MVC opdracht</h1>
+</body>
+</html>
